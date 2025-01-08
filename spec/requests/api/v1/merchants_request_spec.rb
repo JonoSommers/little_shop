d702c98db629d8a52ad19d04996bab3e0cb74de7
Dustin Peukert <dpeukert04@gmail.com>
2025-01-08 14:33:44 -0500

    write tests for missing/blank attributes

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,4 +18,26 @@ describe "Merchants", type: :request do
     expect(new_merchant.id).to be_an(Integer)
     expect(new_merchant.name).to eq(merchant_params[:name])
   end
+
+  it 'will not save a merchant that is missing attributes' do
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/merchants", headers: headers, params: JSON.generate({})
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+
+  it 'will not save a merchant that has blank attributes' do
+    merchant_params = {
+      "name": ""
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
 end
