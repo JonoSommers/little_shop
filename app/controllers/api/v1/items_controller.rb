d94b1912d806181772bbed42b317bb2d1a3791c2
Dustin Peukert <dpeukert04@gmail.com>
2025-01-07 19:04:37 -0500

    write create method for items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,12 @@
-class Api::v1::ItemsController < ApplicationController
+class Api::V1::ItemsController < ApplicationController
+  def create
+    item = Item.create(item_params)
+    render json: ItemSerializer.new(item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
