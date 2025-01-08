899c90a9fc34c37436aba54c693d46c13b98fa70
Dustin Peukert <dpeukert04@gmail.com>
2025-01-07 19:28:23 -0500

    add status code render

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MerchantsController < ApplicationController
   def create
     merchant = Merchant.create(merchant_params)
-    render json: MerchantSerializer.new(merchant)
+    render json: MerchantSerializer.new(merchant), status: :created
   end
 
   private
