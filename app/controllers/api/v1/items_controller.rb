9ddf23d99d5ab643e9c918a553c002101e075b3e
Dustin Peukert <dpeukert04@gmail.com>
2025-01-07 19:26:54 -0500

    add status code render

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def create
     item = Item.create(item_params)
-    render json: ItemSerializer.new(item)
+    render json: ItemSerializer.new(item), status: :created
   end
 
   private
