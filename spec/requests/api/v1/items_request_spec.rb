c9484300cb2f36e39bc5cb4ff3cb596ee3885a57
Dustin Peukert <dpeukert04@gmail.com>
2025-01-07 19:01:27 -0500

    write test for items creation

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Items", type: :request do
+  it 'can create a poster' do
+    merchant = Merchant.create!(name: "Test Merchant")
+    item_params = {
+      "name": "Banana",
+      "description": "a yellow fruit",
+      "unit_price": 0.99,
+      "merchant_id": merchant.id
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    expect(response).to be_successful
+    
+    new_item = Item.last
+
+    expect(new_item.name).to eq(item_params[:name])
+    expect(new_item.description).to eq(item_params[:description])
+    expect(new_item.unit_price).to eq(item_params[:unit_price])
+    expect(new_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+end
