a00957f58658c770e0bdda6249f50e9f160a3373
Dustin Peukert <dpeukert04@gmail.com>
2025-01-08 14:34:16 -0500

    write error handling for missing/blank attributes

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,12 +1,23 @@
 class Api::V1::MerchantsController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :render_unprocessable_entity
+
   def create
-    merchant = Merchant.create(merchant_params)
-    render json: MerchantSerializer.new(merchant), status: :created
+    merchant = Merchant.new(merchant_params)
+    if merchant.valid?
+      merchant.save
+      render json: MerchantSerializer.new(merchant), status: :created
+    else
+      render json: { message: 'Validation Failed', errors: merchant.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
   end
 
   private
 
   def merchant_params
     params.require(:merchant).permit(:name)
+  end
+
+  def render_unprocessable_entity(exception)
+    render json: { message: exception.message, errors: [exception.message] }, status: :unprocessable_entity
   end
 end
