7dfb71ae3b2084b3faa69b08bbd6ec52f9c31654
elysableu <elysabward@gmail.com>
2025-01-07 18:48:33 -0700

    Update tests to address errors

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Merchant endpoints" do
+RSpec.describe "Merchant endpoints", type: :request do
 
   describe "FETCH /merchants/:id" do
     it 'can fetch a single record at a specific id' do
-      id = Merchant.create( name: "Lula Faye" )
+      id = Merchant.create( name: "Lula Faye" ).id.to_s
 
       get "/api/v1/merchants/#{id}"
       
       merchant = JSON.parse(response.body, symbolize_names: true)
-
+      
       expect(response).to be_successful
 
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      merchantData = merchant[:data]
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
-      expect(merchant[:name]).to eq('Lula Faye')
+      expect(merchantData).to have_key(:id)
+      expect(merchantData[:id]).to be_a(String)
+     
+      expect(merchantData[:attributes]).to have_key(:name)
+      expect(merchantData[:attributes][:name]).to be_a(String)
+      expect(merchantData[:attributes][:name]).to eq('Lula Faye')
     end
   end
 end
