f6216a670b11851888e5bfbbead739dde5de14c7
Dustin Peukert <dpeukert04@gmail.com>
2025-01-07 19:02:02 -0500

    write create method for merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,2 +1,12 @@
-class Api::v1::CustomersController < ApplicationController
+class Api::V1::MerchantsController < ApplicationController
+  def create
+    merchant = Merchant.create(merchant_params)
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
