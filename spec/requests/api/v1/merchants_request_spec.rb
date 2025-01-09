7976447e86c62f244b2ab7b8cb244425b574003b
elysableu <elysabward@gmail.com>
2025-01-08 20:08:32 -0700

    Add test to check for merchnat record cascade deleting

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -65,16 +65,30 @@ RSpec.describe "Merchant endpoints", type: :request do
   end
 
   describe "DELETE /merchants/:id" do
-    it 'can delete a select merchant' do
-      merchant1 = Merchant.create( name: "Lula Faye")
-      merchant2 = Merchant.create( name: "Michaels Craft Store")
+    before(:each) do
+      @merchant1 = Merchant.create( name: "Lula Faye")
+      @merchant2 = Merchant.create( name: "Michaels Craft Store")
 
+      @item1 = Item.create(  name: "Crochet Hook", description: "5mm hook", unit_price: 5.99, merchant_id: @merchant1.id )
+      @item2 = Item.create( name: "Cashmere Yarn", description: "A teal green yarn", unit_price: 19.99, merchant_id: @merchant1.id )
+    end
+
+    it 'can delete a select merchant' do
       expect(Merchant.all.length).to eq(2)
 
-      delete "/api/v1/merchants/#{merchant2.id}"
+      delete "/api/v1/merchants/#{@merchant2.id}"
 
       expect(Merchant.all.length).to eq(1)
-      expect{ Merchant.find(merchant2.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect{ Merchant.find(@merchant2.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'will delete all invoices and items when select merchant is deleted' do
+      expect(@merchant1.items).to eq([@item1, @item2])
+      
+      # require 'pry'; binding.pry
+      delete "/api/v1/merchants/#{@merchant1.id}"
+      expect{ Item.find(@item1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect{ Item.find(@item2.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
