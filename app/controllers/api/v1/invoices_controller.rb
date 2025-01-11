3ee0503553b520344d5d0656d82a42ce159487d3
Jono Sommers <jonosommers95@gmail.com>
2025-01-11 15:02:19 -0600

    test for invoice get endpoint complete / passing

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -7,11 +7,12 @@ class Api::V1::InvoicesController < ApplicationController
         if params[:status] == nil
             options[:meta] = {count: merchant_invoices.count}
             render json: InvoiceSerializer.new(merchant_invoices, options)
-        else
+        elsif
             target_invoices = invoices.where(merchant_id: params[:merchant_id], status: params[:status])
             options[:meta] = {count: target_invoices.count}
-            # binding.pry
             render json: InvoiceSerializer.new(target_invoices, options)
+        else
+            render json: { error: target_invoices.errors.full_message }, status: :not_found
         end
     end
 end
---
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe "Invoice endpoints", type: :request do
+    describe 'Invoices' do
+        it 'renders a JSON representation of all invoices for a given merchant that have a status matching the desired status query parameter' do
+            
+            customer = Customer.create(
+                first_name: 'Cust',
+                last_name: 'Omer'
+            )
+
+            merchant1 = Merchant.create(
+                name: 'Jono'
+            )
+
+            merchant2 = Merchant.create(
+                name: 'Dustin'
+            )
+
+            merchant3 = Merchant.create(
+                name: 'Elysa'
+            )
+
+            invoice1 = Invoice.create(
+                customer_id: customer.id,
+                merchant_id: merchant1.id,
+                status: 'shipped'
+            )
+
+            invoice2 = Invoice.create(
+                customer_id: customer.id,
+                merchant_id: merchant1.id,
+                status: 'shipped'
+            )
+
+            invoice3 = Invoice.create(
+                customer_id: customer.id,
+                merchant_id: merchant2.id,
+                status: 'returned'
+            )
+
+            invoice4 = Invoice.create(
+                customer_id: customer.id,
+                merchant_id: merchant3.id,
+                status: 'packaged'
+            )
+
+            get "/api/v1/merchants/#{merchant1.id}/invoices?status=shipped"
+            expect(response).to be_successful
+            invoices = JSON.parse(response.body, symbolize_names: true)
+            expect(invoices[:data].length).to eq(2)
+
+            get "/api/v1/merchants/#{merchant2.id}/invoices?status=returned"
+            expect(response).to be_successful
+            invoices = JSON.parse(response.body, symbolize_names: true)
+            expect(invoices[:data].length).to eq(1)
+
+            get "/api/v1/merchants/#{merchant3.id}/invoices?status=packaged"           
+            expect(response).to be_successful
+            invoices = JSON.parse(response.body, symbolize_names: true)
+            expect(invoices[:data].length).to eq(1)
+        end
+    end
+end
