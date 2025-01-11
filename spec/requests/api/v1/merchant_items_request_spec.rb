55f75561bdf7e1b49c95c3257d1323607fd0aa71
elysableu <elysabward@gmail.com>
2025-01-11 16:51:23 -0700

    Add test to see that /merchants/:id/items returns merchants items

---
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant_Items:', type: :request do
+  describe "Get /merchants/:id/items" do
+    before(:each) do
+      @merchant1 = Merchant.create( name: "Lula Faye")
+      @item1 = Item.create(  name: "Crochet Hook", description: "5mm hook", unit_price: 5.99, merchant_id: @merchant1.id )
+      @item2 = Item.create( name: "Cashmere Yarn", description: "A teal green yarn", unit_price: 19.99, merchant_id: @merchant1.id )
+      @items_array = [@item1.id, @item2.id]
+    end
+
+    it 'can return all items associated with a merchant' do
+      get "/api/v1/merchants/#{@merchant1.id}/items"
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(2)
+
+      items[:data].each do |item| 
+        expect(item[:id].to_i).to satisfy { |id| @items_array.include?(id) }
+      end
+    end
+
+    xit 'returns a 404 status code when merchant is not found' do
+      
+
+      
+    end
+  end
+end
