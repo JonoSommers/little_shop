12b04c3dc709155f594b5f833b6d73bedaca7231
Jono Sommers <jonosommers95@gmail.com>
2025-01-11 16:01:42 -0600

    working on sad path

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,18 +1,16 @@
 class Api::V1::InvoicesController < ApplicationController
-
     def index
         invoices = Invoice.all
         options = {}
         merchant_invoices = invoices.where(merchant_id: params[:merchant_id])
-        if params[:status] == nil
+        if !params[:status].present?
             options[:meta] = {count: merchant_invoices.count}
             render json: InvoiceSerializer.new(merchant_invoices, options)
-        elsif
+
+        elsif params[:status].present?
             target_invoices = invoices.where(merchant_id: params[:merchant_id], status: params[:status])
             options[:meta] = {count: target_invoices.count}
             render json: InvoiceSerializer.new(target_invoices, options)
-        else
-            render json: { error: target_invoices.errors.full_message }, status: :not_found
         end
     end
 end
