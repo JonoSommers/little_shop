e3af94916438b3566fdbd3d4ccd61a90af61af00
Jono Sommers <jonosommers95@gmail.com>
2025-01-11 12:53:50 -0600

    route created, index method created in invoice controller / happy path test passing on postman

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::InvoicesController < ApplicationController
 
     def index
+        invoices = Invoice.all
+        merchants = Merchant.all
+        options = {}
+        options[:meta] = {count: invoices.count}
+        invoices = Invoice.joins(:merchant).where(merchant: {id: params[:merchant_id]}).distinct
+        invoices = invoices.where(status: params[:status])
+        render json: InvoiceSerializer.new(invoices, options)
     end
 end
