a3a8cb5ae4405c73e1dbfcc3e06634d309c3e2f5
elysableu <elysabward@gmail.com>
2025-01-11 17:31:59 -0700

    Update routes to include /items/:id/merchant endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
   get "/api/v1/items/:id", to: "api/v1/items#show"
   post "/api/v1/items", to: "api/v1/items#create"
   delete "/api/v1/items/:id", to: "api/v1/items#destroy"
+  get "/api/v1/items/:id/merchant", to: "api/v1/items_merchant#index#"
 
   get "/api/v1/merchants", to: "api/v1/merchants#index"
   get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
   post "/api/v1/merchants", to: "api/v1/merchants#create"
   delete "/api/v1/merchants/:id", to: "api/v1/merchants#destroy"
-
   get "/api/v1/merchants/:id/items", to: "api/v1/merchant_items#index"
 end
