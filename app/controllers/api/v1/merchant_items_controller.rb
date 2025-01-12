770dfc13fb131beefbaf96337ee54cc340b08bfa
elysableu <elysabward@gmail.com>
2025-01-11 17:10:43 -0700

    Add error handling for when merchant id is not found

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::MerchantItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
   def index 
       merchant = Merchant.find(params[:id])
       options = {}
       options[:meta] = {count: merchant.items.count}
 
       render json: ItemSerializer.new(merchant.items, options)
+  end
+
+  private
+
+  def record_not_found(exception)
+    render json: { message: exception.message, errors: [exception.message] }, status: :not_found
   end
 end
